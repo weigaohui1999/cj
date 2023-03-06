a8e166a0a899b63913a4270aba759a68755e8093
wei.gaohui <8568705+weigaohui@user.noreply.gitee.com>
2023-03-06 11:47:59 +0800

    add

---
+++ src/components/common/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer text-14 f-c-c flex-col color="#6a6a6a">
+    <p>
+      Copyright©2023
+      <a href="https://github.com/zclzone" target="__blank" hover="decoration-underline color-primary"> 大脸怪</a>
+    </p>
+    <p>
+      <a href="http://beian.miit.gov.cn/" target="__blank" hover="decoration-underline color-primary"> </a>
+    </p>
+  </footer>
+</template>
